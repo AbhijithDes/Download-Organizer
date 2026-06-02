625762a71f431789c79356b674a085232e19b0ce
Abhijith C S <abhijith6cs@gmail.com>
2026-06-02 23:56:44 +0530

    Initial file organizer

---
+++ organizer.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import shutil
+
+DOWNLOADS = Path.home() / "Downloads"
+
+rules = {
+    ".pdf": "Documents",
+    ".zip": "Archives",
+    ".rar": "Archives",
+    ".png": "Images",
+    ".jpg": "Images",
+    ".jpeg": "Images",
+    ".gif": "Images",
+    ".mp4": "Videos",
+    ".mkv": "Videos",
+    ".exe": "Installers",
+    ".iso": "ISOs"
+}
+
+for file in DOWNLOADS.iterdir():
+
+    if file.is_dir():
+        continue
+
+    extension = file.suffix.lower()
+
+    if extension in rules:
+
+        destination_folder = DOWNLOADS / rules[extension]
+
+        destination_folder.mkdir(exist_ok=True)
+
+        shutil.move(
+            str(file),
+            str(destination_folder / file.name)
+        )
+
+        print(f"Moved: {file.name}")
