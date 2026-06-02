2ae8cf62eb5ba064c996599b88aa8d0714ea2e1a
Abhijith C S <abhijith6cs@gmail.com>
2026-06-03 00:38:22 +0530

    Added Real time download monitering

--- organizer.py
+++ organizer.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
 
 DOWNLOADS = Path.home() / "Downloads"
 
@@ -17,23 +20,59 @@ rules = {
     ".iso": "ISOs"
 }
 
-for file in DOWNLOADS.iterdir():
 
-    if file.is_dir():
-        continue
+class DownloadHandler(FileSystemEventHandler):
 
-    extension = file.suffix.lower()
+    def on_created(self, event):
 
-    if extension in rules:
-        destination_folder = DOWNLOADS / rules[extension]
-    else:
-        destination_folder = DOWNLOADS / "Miscellaneous"
+        if event.is_directory:
+            return
 
+        file = Path(event.src_path)
+
+        extension = file.suffix.lower()
+
+        # Decide destination folder
+        if extension in rules:
+            destination_folder = DOWNLOADS / rules[extension]
+        else:
+            destination_folder = DOWNLOADS / "Miscellaneous"
+
+        # Create folder if it doesn't exist
         destination_folder.mkdir(exist_ok=True)
 
-        shutil.move(
-            str(file),
-            str(destination_folder / file.name)
-        )
+        # Small delay so file is fully downloaded
+        time.sleep(1)
 
-        print(f"Moved: {file.name}")
+        try:
+            shutil.move(
+                str(file),
+                str(destination_folder / file.name)
+            )
+
+            print(f"Moved: {file.name} -> {destination_folder.name}")
+
+        except Exception as e:
+            print(f"Error moving {file.name}: {e}")
+
+
+observer = Observer()
+
+observer.schedule(
+    DownloadHandler(),
+    str(DOWNLOADS),
+    recursive=False
+)
+
+observer.start()
+
+print("Monitoring Downloads folder... (Press Ctrl + C to stop)")
+
+try:
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
